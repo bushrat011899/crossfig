17389b0888bd263fd80466ea2e4654b266c760c6
Zac Harrold <zac@harrold.com.au>
2025-06-22 10:20:32 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,151 @@
+# `crossfig`
+
+Provides two macros to assist with managing conditional compilation in Rust.
+
+- `no_std`
+- _No_ `build.rs`
+- _Zero_ features
+- _Zero_ dependencies
+- _No_ `proc-macros`
+
+Instead, this crate defines 4 `macro_rules` macros:
+
+- `switch`
+- `alias`
+- `enabled`
+- `disabled`
+
+## Tl;Dr
+
+```rust
+crossfig::alias! {
+    /// Indicates whether the `std` feature is enabled.
+    pub std: { #[cfg(feature = "std")] }
+}
+
+crossfig::switch! {
+    std => {
+        extern crate std;
+        std::println!("Can use std!");
+    }
+    _ => {
+        // no_std
+    }
+}
+```
+
+## `enabled` and `disabled`
+
+`enabled` and `disabled` are the simplest to explain: they either pass their contents unchanged _or_ suppress them.
+
+```rust
+crossfig::enabled! {
+    println!("I will print!");
+}
+
+crossfig::disabled! {
+    compile_error!("I won't be triggered!");
+}
+```
+
+If no content is provided to either macro, an appropriate `bool` value will be returned, allowing use in `if` statements and other logical operations.
+
+```rust
+if crossfig::enabled!() {
+    println!("I will print!");
+}
+
+if crossfig::disabled!() {
+    println!("I won't print!");
+}
+```
+
+## `alias`
+
+On their own, these macros aren't very helpful.
+That's where `alias` comes in.
+The `alias` macro will `use` either `enabled` or `disabled` with a new provided identifier based on an arbitrary conditional compilation configuration.
+
+```rust
+crossfig::alias! {
+    std: { #[cfg(feature = "std")] }
+}
+
+// Is roughly equivalent to:
+#[cfg(feature = "std")]
+use crossfig::enabled as std;
+#[cfg(not(feature = "std"))]
+use crossfig::disabled as std;
+```
+
+Since the aliases created by `alias` are `macro_rules` items, they can be documented and even _publicly exported_.
+
+```rust
+crossfig::alias! {
+    /// Indicates the log feature is enabled.
+    pub log: { #[cfg(feature = "log")] }
+}
+```
+
+When publicly exported, it's important to understand that an alias is evaluated as either `enabled` or `disabled` at the _definition_ site, not the _call_ site.
+This means aliases can be used to determine what features are enabled in _dependencies_.
+
+```rust
+// In a crate `foo`
+crossfig::alias! {
+    /// Indicates the log feature is enabled.
+    pub log: { #[cfg(feature = "log")] }
+}
+
+// In a consuming crate
+if foo::log!() {
+    // `foo`'s `log` feature has been enabled!
+}
+```
+
+Note that aliases and standard `#[cfg(...)]` attributes can be mixed and matched within definitions, and combined with `not`, `any` and `all` operators:
+
+```rust
+crossfig::alias! {
+    a: { #[cfg(feature = "a")] }
+    b: { #[cfg(feature = "b")] }
+    c: { all(a, not(b), #[cfg(feature = "c")]) }
+}
+```
+
+While aliases are powerful, they still don't solve a common issue: ranked choice.
+It's common to have multiple features in a crate which all contribute to a single choice.
+For example, you may have a `parking_lot`, `std` and `spin` set of features to choose what `Mutex` implementation is used internally.
+
+## `switch`
+
+To make this particular problem nicer, we use the final macro in this crate, `switch`:
+
+```rust
+crossfig::alias! {
+    parking_lot: { #[cfg(feature = "parking_lot")] }
+    std: { #[cfg(feature = "std")] }
+    spin: { #[cfg(feature = "spin")] }
+}
+
+crossfig::switch! {
+    parking_lot => {
+        use parking_lot::Mutex;
+    }
+    std => {
+        use std::sync::Mutex;
+    }
+    spin => {
+        use spin::Mutex;
+    }
+    _ => {
+        compile_error!("Must select a `Mutex` provider!");
+    }
+}
+```
+
+## MSRV
+
+The minimum supported Rust version for this crate is 1.6.0, with the 2015 edition, allowing it to be used in virtually any Rust project.
+Note that support for 1.0.0 is blocked by the unavailability of `#![no_std]`.
+If support for these earlier versions of Rust would help you, please create an issue!
---
+++ src/lib.rs
@@ -0,0 +1,812 @@
+//! Provides helpful configuration macros, allowing features to be introspected
+//! across crates at compile time.
+//!
+//! The two main features that power this crate are [`switch`] and [`alias`].
+//!
+//! # `switch`
+//!
+//! [`switch`] provides a `match`-like syntax to make complex compile-time configuration
+//! more ergonomic.
+//!
+//! ```standalone_crate
+//! // Without `crossfig`:
+//!
+//! #[cfg(feature = "fastest")]
+//! // Use the fastest algorithm!
+//!
+//! #[cfg(all(not(feature = "fastest"), feature = "fast"))]
+//! // Use the fast algorithm
+//!
+//! #[cfg(all(not(feature = "fastest"), not(feature = "fast")))]
+//! // Use the slow algorithm
+//! ```
+//!
+//! ```standalone_crate
+//! // With `crossfig`:
+//!
+//! crossfig::switch! {
+//!     #[cfg(feature = "fastest")] => {
+//!         // Use the fastest algorithm!
+//!     }
+//!     #[cfg(feature = "fast")] => {
+//!         // Use the fast algorithm
+//!     }
+//!     _ => {
+//!         // Use the slow algorithm
+//!     }
+//! }
+//! ```
+//!
+//! # `alias`
+//!
+//! [`alias`] instead allows for creating a short-hand for common configurations.
+//!
+//! ```standalone_crate
+//! # use crossfig::alias;
+//! // Define an alias
+//! alias! {
+//!     std: { #[cfg(feature = "std")] }
+//! }
+//!
+//! // Use it for conditional compilation
+//! std! {
+//!     // This code is only compiled with feature = "std"
+//! #   ()
+//! }
+//! ```
+//!
+//! # Combined
+//!
+//! Individually, both are useful tools for reducing repetition in conditional compilation.
+//! But where they really shine is when combined:
+//!
+//! ```standalone_crate
+//! # use crossfig::{alias, switch};
+//! alias! {
+//!     std: { #[cfg(feature = "std")] },
+//!     log: { #[cfg(feature = "log")] },
+//! }
+//!
+//! switch! {
+//!     log => {
+//!         // Use the log crate
+//!     }
+//!     std => {
+//!         // Use println! for logging
+//!     }
+//!     _ => {
+//!         // Don't bother logging
+//!     }
+//! }
+//! ```
+//!
+//! # Cross-Crate Configuration Introspection
+//!
+//! One last trick; aliases created with [`alias`] can be _exported_ in
+//! your public API.
+//!
+//! ```standalone_crate
+//! // In the `foo` crate:
+//! # mod foo {
+//! crossfig::alias! {
+//!     pub multi_threading: { #[cfg(feature = "multi_threading")] }
+//! }
+//! # }
+//!
+//! // In a consumer's crate:
+//! # mod bar {
+//! # use super::foo;
+//! foo::multi_threading! {
+//!     // This code will be compiled if and only if foo/multi_threading
+//!     // is enabled.
+//! #   ()
+//! }
+//! # }
+//! #
+//! # fn main() {}
+//! ```
+//!
+//! This can be extremely powerful, as it allows _consuming_ crates to introspect
+//! on the enabled features of their _dependencies_.
+//!
+//! # `macro_rules_attribute`
+//!
+//! As a quality of life improvement, you can also use [`macro_rules_attribute`].
+//! This provides an attribute proc-macro `apply`, which can be used to apply aliases
+//! to items as an attribute.
+//!
+//! ```ignore
+//! #![no_std]
+//!
+//! use macro_rules_attribute::apply;
+//!
+//! alias! {
+//!     std: { #[cfg(feature = "std")] }
+//! }
+//!
+//! #[apply(std)]
+//! extern crate std;
+//! ```
+//!
+//! [`macro_rules_attribute`]: https://docs.rs/macro_rules_attribute
+
+#![no_std]
+#![no_implicit_prelude]
+#![forbid(unsafe_code)]
+#![cfg_attr(crossfig_no_core, feature(no_core))]
+#![cfg_attr(crossfig_no_core, no_core)]
+
+/// Provides a `match`-like expression similar to [`cfg_if`] and based on the experimental
+/// [`cfg_match`].
+/// The name `switch` is used to avoid conflict with the `match` keyword.
+/// Arms are evaluated top to bottom, and an optional wildcard arm can be provided if no match
+/// can be made.
+///
+/// An arm can either be:
+/// - a `cfg(...)` pattern (e.g., `feature = "foo"`)
+/// - a wildcard `_`
+/// - an alias defined using [`alias`]
+///
+/// Note that aliases are evaluated from the context of the defining crate, not the consumer.
+/// This allows a library to export aliases for use in consuming crates.
+///
+/// # Examples
+///
+/// ```
+/// # use crossfig::{switch, alias};
+/// # fn log<T>(_: T) {}
+/// # fn foo<T>(_: T) {}
+/// # alias! {
+/// #    std: { true }
+/// # }
+/// #
+/// switch! {
+///     #[cfg(feature = "foo")] => {
+///         foo("We have the `foo` feature!")
+///     }
+///     std => {
+///         extern crate std;
+///         std::println!("No `foo`, but we have `std`!");
+///     }
+///     _ => {
+///         log("Don't have `std` or `foo`");
+///     }
+/// }
+/// ```
+///
+/// [`cfg_if`]: https://crates.io/crates/cfg-if
+/// [`cfg_match`]: https://github.com/rust-lang/rust/issues/115585
+#[macro_export]
+macro_rules! switch {
+    // Allow switch!{{ ... }} to act as an expression in certain contexts
+    (
+        { $($tt:tt)* }
+    ) => {
+        {
+            $crate::switch! { $($tt)* }
+        }
+    };
+
+    // Common mistake: arms after wildcard
+    (
+        _ => $output:tt
+        $( $rest:tt )+
+    ) => {
+        compile_error!(concat!("patterns after a wildcard are ignored: `", stringify!($($cond)+), "`"));
+    };
+
+    // Wildcard branch
+    (
+        _ => $output:tt
+    ) => {
+        $output
+    };
+
+    // #[cfg(...)] integration
+    (
+        #[cfg($cfg:meta)] => $output:tt
+    ) => {
+        $crate::eval!(
+            { $crate::switch! { _ => $output } }
+            { }
+            { #[cfg($cfg)] }
+        )
+    };
+    (
+        #[cfg($cfg:meta)] => $output:tt
+        $( $rest:tt )+
+    ) => {
+        $crate::eval!(
+            { $crate::switch! { _ => $output } }
+            { $crate::switch! { $($rest)+ } }
+            { #[cfg($cfg)] }
+        )
+    };
+
+    // ops integration
+    (
+        $op:ident($($args:tt)*) => $output:tt
+    ) => {
+        $crate::eval!(
+            { $crate::switch! { _ => $output } }
+            { }
+            { $op($($args)*) }
+        )
+    };
+    (
+        $op:ident($($args:tt)*) => $output:tt
+        $( $rest:tt )+
+    ) => {
+        $crate::eval!(
+            { $crate::switch! { _ => $output } }
+            { $crate::switch! { $($rest)+ } }
+            { $op($($args)*) }
+        )
+    };
+
+    // alias integration
+    (
+        $cond:path => $output:tt
+    ) => {
+        $crate::eval!(
+            { $crate::switch! { _ => $output } }
+            { }
+            { $cond }
+        )
+    };
+    (
+        $cond:path => $output:tt
+        $( $rest:tt )+
+    ) => {
+        $crate::eval!(
+            { $crate::switch! { _ => $output } }
+            { $crate::switch! { $($rest)+ } }
+            { $cond }
+        )
+    };
+}
+
+/// # Examples
+///
+/// ## As a `boolean`
+///
+/// ```
+/// assert_eq!(crossfig::disabled!(), false);
+/// ```
+///
+/// ## As a Conditional Compilation Guard
+///
+/// ```
+/// crossfig::disabled! {
+///     use a_crate_that_is_not_available::*;
+///     // ...
+/// }
+/// ```
+///
+/// ## As an `if`-Statement
+///
+/// ```
+/// crossfig::disabled! {
+///     if {
+///         let was_disabled = false;
+///     } else {
+///         let was_disabled = true;
+///     }
+/// }
+///
+/// assert!(was_disabled);
+/// ```
+#[macro_export]
+macro_rules! disabled {
+    () => { false };
+    (if { $($p:tt)* } else { $($n:tt)* }) => { $($n)* };
+    ($($p:tt)*) => {};
+}
+
+/// # Examples
+///
+/// ## As a `boolean`
+///
+/// ```
+/// assert_eq!(crossfig::enabled!(), true);
+/// ```
+///
+/// ## As a Conditional Compilation Guard
+///
+/// ```
+/// crossfig::enabled! {
+/// # /*
+///     use a_crate_that_is_available::*;
+/// # */
+///     // ...
+/// #   ()
+/// }
+/// ```
+///
+/// ## As an `if`-Statement
+///
+/// ```
+/// crossfig::enabled! {
+///     if {
+///         let was_enabled = true;
+///     } else {
+///         let was_enabled = false;
+///     }
+/// }
+///
+/// assert!(was_enabled);
+/// ```
+#[macro_export]
+macro_rules! enabled {
+    () => { true };
+    (if { $($p:tt)* } else { $($n:tt)* }) => { $($p)* };
+    ($($p:tt)*) => { $($p)* };
+}
+
+/// Defines an alias for a particular configuration.
+/// This has two advantages over directly using `#[cfg(...)]`:
+///
+/// 1. Complex configurations can be abbreviated to more meaningful shorthand.
+/// 2. Features are evaluated in the context of the _defining_ crate, not the consuming.
+///
+/// The second advantage is a particularly powerful tool, as it allows consuming crates to use
+/// functionality in a defining crate regardless of what crate in the dependency graph enabled the
+/// relevant feature.
+///
+/// For example, consider a crate `foo` that depends on another crate `bar`.
+/// `bar` has a feature "`faster_algorithms`".
+/// If `bar` defines a "`faster_algorithms`" alias:
+///
+/// ```
+/// # use crossfig::alias;
+/// alias! {
+///     pub faster_algorithms: {
+///         #[cfg(feature = "faster_algorithms")]
+///     }
+/// }
+/// ```
+///
+/// Now, `foo` can gate its usage of those faster algorithms on the alias, avoiding the need to
+/// expose its own "`faster_algorithms`" feature.
+/// This also avoids the unfortunate situation where one crate activates "`faster_algorithms`" on
+/// `bar` without activating that same feature on `foo`.
+///
+/// Once an alias is defined, there are 4 ways you can use it:
+///
+/// 1. Evaluate with no contents to return a `bool` indicating if the alias is active.
+///    ```
+///    # use crossfig::alias;
+///    # alias! {
+///    #    std: { true }
+///    # }
+///    if std!() {
+///        // Have `std`!
+///    } else {
+///        // No `std`...
+///    }
+///    ```
+/// 2. Pass a single code block which will only be compiled if the alias is active.
+///    ```
+///    # use crossfig::alias;
+///    # alias! {
+///    #    std: { true }
+///    # }
+///    std! {
+///        // Have `std`!
+///    # ()
+///    }
+///    ```
+/// 3. Pass a single `if { ... } else { ... }` expression to conditionally compile either the first
+///    or the second code block.
+///    ```
+///    # use crossfig::alias;
+///    # alias! {
+///    #    std: { true }
+///    # }
+///    std! {
+///        if {
+///            // Have `std`!
+///        } else {
+///            // No `std`...
+///        }
+///    }
+///    ```
+/// 4. Use in a [`switch`] arm for more complex conditional compilation.
+///    ```
+///    # use crossfig::{alias, switch};
+///    # alias! {
+///    #    std: { true }
+///    # }
+///    switch! {
+///        std => {
+///            // Have `std`!
+///        }
+///        alloc => {
+///            // No `std`, but do have `alloc`!
+///        }
+///        _ => {
+///            // No `std` or `alloc`...
+///        }
+///    }
+///    ```
+#[macro_export]
+macro_rules! alias {
+    () => {};
+    (
+        $(#[$p_meta:meta])*
+        $vis:vis $p:ident: { $($cond:tt)+ }
+    ) => {
+        $crate::alias! {
+            $(#[$p_meta])*
+            $vis $p: { $($cond)+ },
+        }
+    };
+    (
+        $(#[$p_meta:meta])*
+        $vis:vis $p:ident: { $($cond:tt)+ },
+
+        $($rest:tt)*
+    ) => {
+        $crate::eval!(
+            {
+                $(#[$p_meta])*
+                #[doc(inline)]
+                ///
+                #[doc = concat!("This macro passes the provided code because `", stringify!($($cond)+), "` is currently active.")]
+                $vis use $crate::enabled as $p;
+            }
+            {
+                $(#[$p_meta])*
+                #[doc(inline)]
+                ///
+                #[doc = concat!("This macro suppresses the provided code because `", stringify!($($cond)+), "` is _not_ currently active.")]
+                $vis use $crate::disabled as $p;
+            }
+            { $($cond)+ }
+        );
+
+        $crate::alias! {
+            $($rest)*
+        }
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! eval {
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { true }) => {
+        $($truthy)*
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { false }) => {
+        $($falsy)*
+    };
+
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { _ }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            { $($falsy)* }
+            { true }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { all() }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            { $($falsy)* }
+            { true }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { any() }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            { $($falsy)* }
+            { false }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { #[cfg($meta:meta)] }) => {
+        #[cfg($meta)]
+        $crate::eval!(
+            { $($truthy)* }
+            { $($falsy)* }
+            { true }
+        );
+
+        #[cfg(not($meta))]
+        $crate::eval!(
+            { $($truthy)* }
+            { $($falsy)* }
+            { false }
+        );
+    };
+
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { not($($cond:tt)*) }) => {
+        $crate::eval!(
+            { $($falsy)* }
+            { $($truthy)* }
+            { $($cond)* }
+        );
+    };
+
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { all(#[cfg($meta:meta)]) }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            { $($falsy)* }
+            { all(#[cfg($meta)],) }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { all($op:ident($($cond:tt)*)) }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            { $($falsy)* }
+            { all($op($($cond)*),) }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { all($cond:path) }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            { $($falsy)* }
+            { all($cond,) }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { all(#[cfg($meta:meta)], $($rest:tt)*) }) => {
+        $crate::eval!(
+            {
+                $crate::eval!(
+                    { $($truthy)* }
+                    { $($falsy)* }
+                    { all($($rest)*) }
+                );
+            }
+            { $($falsy)* }
+            { #[cfg($meta)] }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { all($op:ident($($cond:tt)*), $($rest:tt)*) }) => {
+        $crate::eval!(
+            {
+                $crate::eval!(
+                    { $($truthy)* }
+                    { $($falsy)* }
+                    { all($($rest)*) }
+                );
+            }
+            { $($falsy)* }
+            { $op($($cond)*) }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { all($cond:path, $($rest:tt)*) }) => {
+        $crate::eval!(
+            {
+                $crate::eval!(
+                    { $($truthy)* }
+                    { $($falsy)* }
+                    { all($($rest)*) }
+                );
+            }
+            { $($falsy)* }
+            { $cond }
+        );
+    };
+
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { any(#[cfg($meta:meta)]) }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            { $($falsy)* }
+            { any(#[cfg($meta)],) }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { any($op:ident($($cond:tt)*)) }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            { $($falsy)* }
+            { any($op($($cond)*),) }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { any($cond:path) }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            { $($falsy)* }
+            { any($cond,) }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { any(#[cfg($meta:meta)], $($rest:tt)*) }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            {
+                $crate::eval!(
+                    { $($truthy)* }
+                    { $($falsy)* }
+                    { any($($rest)*) }
+                );
+            }
+            { #[cfg($meta)] }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { any($op:ident($($cond:tt)*), $($rest:tt)*) }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            {
+                $crate::eval!(
+                    { $($truthy)* }
+                    { $($falsy)* }
+                    { any($($rest)*) }
+                );
+            }
+            { $op($($cond)*) }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { any($cond:path, $($rest:tt)*) }) => {
+        $crate::eval!(
+            { $($truthy)* }
+            {
+                $crate::eval!(
+                    { $($truthy)* }
+                    { $($falsy)* }
+                    { any($($rest)*) }
+                );
+            }
+            { $cond }
+        );
+    };
+    ({ $($truthy:tt)* } { $($falsy:tt)* } { $cond:path }) => {
+        $cond! {
+            if {
+                $crate::eval!(
+                    { $($truthy)* }
+                    { $($falsy)* }
+                    { true }
+                );
+            } else {
+                $crate::eval!(
+                    { $($truthy)* }
+                    { $($falsy)* }
+                    { false }
+                );
+            }
+        }
+    };
+}
+
+#[cfg(test)]
+mod alias_tests {
+    use super::alias;
+
+    alias! {
+        a: { #[cfg(all())] },
+        b: { #[cfg(all())] },
+        c: { a },
+        d: { all(a, b, c) },
+        e: { any(not(a), b, all(c), #[cfg(test)]) },
+        pub f: { e },
+    }
+}
+
+#[cfg(test)]
+mod eval_tests {
+    use super::{disabled, enabled, eval};
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { true }
+    );
+
+    eval!(
+        { compile_error!("expected falsy"); }
+        { }
+        { false }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { _ }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { all() }
+    );
+
+    eval!(
+        { compile_error!("expected falsy"); }
+        { }
+        { any() }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { #[cfg(test)] }
+    );
+
+    eval!(
+        { compile_error!("expected falsy"); }
+        { }
+        { not(#[cfg(test)]) }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { any(#[cfg(test)]) }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { all(#[cfg(test)]) }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { enabled }
+    );
+
+    eval!(
+        { compile_error!("expected falsy"); }
+        { }
+        { not(enabled) }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { any(enabled) }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { all(enabled) }
+    );
+
+    eval!(
+        { compile_error!("expected falsy"); }
+        { }
+        { disabled }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { not(disabled) }
+    );
+
+    eval!(
+        { compile_error!("expected falsy"); }
+        { }
+        { any(disabled) }
+    );
+
+    eval!(
+        { compile_error!("expected falsy"); }
+        { }
+        { all(disabled) }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { any(disabled, disabled, enabled) }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { all(enabled, enabled, enabled) }
+    );
+
+    eval!(
+        { compile_error!("expected falsy"); }
+        { }
+        { not(not(disabled)) }
+    );
+
+    eval!(
+        { }
+        { compile_error!("expected truthy"); }
+        { all(any(any(not(disabled), enabled, disabled))) }
+    );
+}
